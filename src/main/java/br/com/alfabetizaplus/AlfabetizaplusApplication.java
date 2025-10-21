8731e8205c1f6471887fd44f28ef8f0723d78926
rafael <rlag@outlook.com>
2025-10-20 22:15:50 -0300

    Start Project

---
+++ src/main/java/br/com/alfabetizaplus/AlfabetizaplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.alfabetizaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabetizaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfabetizaplusApplication.class, args);
+	}
+
+}
