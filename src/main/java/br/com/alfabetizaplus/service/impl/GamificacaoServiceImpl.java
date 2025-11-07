851884550d5ec25476dbe03553ca74a8a311497b
rafael <rlag@outlook.com>
2025-11-07 01:42:07 -0300

    Correção de erro

--- src/main/java/br/com/alfabetizaplus/service/impl/GamificacaoServiceImpl.java
+++ src/main/java/br/com/alfabetizaplus/service/impl/GamificacaoServiceImpl.java
@@ -18,7 +18,7 @@ public class GamificacaoServiceImpl implements GamificacaoService {
 
     @Override
     public Gamificacao obterGamificacaoPorUsuario(Usuario usuario) {
-        return gamificacaoRepository.findByUsuario(usuario.getId())
+        return gamificacaoRepository.findByUsuario(usuario)
                 .orElseThrow(() -> new RuntimeException("Gamificação não encontrada para o usuário."));
     }
 
